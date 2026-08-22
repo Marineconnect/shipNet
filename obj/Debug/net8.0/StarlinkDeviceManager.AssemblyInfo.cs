46d5e6485846bc6a18de824e35d0d59cc3c42a5a
nguyenduc216 <duc.dhqn@gmail.com>
2026-08-23 04:08:46 +0700

    commit code

--- obj/Debug/net8.0/StarlinkDeviceManager.AssemblyInfo.cs
+++ obj/Debug/net8.0/StarlinkDeviceManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarlinkDeviceManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a83c34ef58792a089e73682e21bc6a6f474feda2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204efe306ea1463814c8c8e14f9ea49157fcf0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarlinkDeviceManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarlinkDeviceManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
